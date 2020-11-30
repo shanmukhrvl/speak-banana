c102f352ed0442b88885751c928c8e5ba4ed7ae6
shanmukhrvl <teja.ravella@gmail.com>
2020-11-30 19:45:42 +0530

    feat: text translates to minion language

--- scripts.js
+++ scripts.js
@@ -2,9 +2,28 @@ const btnTranslate = document.querySelector("#btn-translate");
 const txtInput = document.querySelector("#txt-input");
 const outputDiv = document.querySelector("#output");
 
+const serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input;
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("Something wrong with server, try again after some time");
+}
 
 function clickHandler() {
-    outputDiv.innerText = txtInput.value;
+    const inputText = txtInput.value;
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            const translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler);
 }
 
 btnTranslate.addEventListener("click", clickHandler);
